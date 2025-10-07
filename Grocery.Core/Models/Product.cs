f34c11eacd4a737ed1d98dcc1a1fe06d81fe439a
Carlos <79849894+6Waifu9@users.noreply.github.com>
2025-10-07 19:29:15 +0200

    prijs toegevoegd aan repository

--- Grocery.Core/Models/Product.cs
+++ Grocery.Core/Models/Product.cs
@@ -5,18 +5,30 @@ namespace Grocery.Core.Models
     public partial class Product : Model
     {
         [ObservableProperty]
-        public int stock;
-        public DateOnly ShelfLife { get; set; }
-        public Product(int id, string name, int stock) : this(id, name, stock, default) { }
+        private int stock;
 
-        public Product(int id, string name, int stock, DateOnly shelfLife) : base(id, name) 
+        [ObservableProperty]
+        private decimal price; // 👈 nieuwe property
+
+        public DateOnly ShelfLife { get; set; }
+
+        public Product(int id, string name, int stock)
+            : this(id, name, stock, 0m, default) { }
+
+        public Product(int id, string name, int stock, decimal price)
+            : this(id, name, stock, price, default) { }
+
+        public Product(int id, string name, int stock, decimal price, DateOnly shelfLife)
+            : base(id, name)
         {
             Stock = stock;
+            Price = price;
             ShelfLife = shelfLife;
         }
+
         public override string? ToString()
         {
-            return $"{Name} - {Stock} op voorraad";
+            return $"{Name} - {Stock} op voorraad - €{Price:F2}";
         }
     }
 }
